4a430c8399976507aaf2e96037eb18d2576e84eb
gitcptyou <2705914@collegelacite.ca>
2023-08-01 10:46:44 -0400

    seconde modification

---
+++ app/contact/page.js
@@ -0,0 +1,96 @@
+"use client"
+import React from "react";
+import { useState } from "react";
+const contact = () => {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    const nomRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+    const prenomRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+    const descriptionRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+
+    const [state, setState] = useState({
+        prenom: '',
+        nom: '',
+        email: '',
+        description:''
+       
+    })
+
+    const [errors, setErrors] = useState({
+        prenom: '',
+        nom: '',
+        age: '',
+        description: '',
+      
+      })
+
+      function validateField(field, value) {
+        switch (field) {
+            case 'prenom':
+            case 'nom':
+            case 'description':
+                if (!nomRegex.test(value)) setErrors(prev => ({ ...prev, [field]: `${field} n'est pas valide` }))
+                else setErrors(prev => ({ ...prev, [field]: '' }))
+                break
+            case 'email':
+                if (!emailRegex.test(value)) setErrors(prev => ({ ...prev, [field]: `email n'est pas valide` }))
+                else setErrors(prev => ({ ...prev, [field]: '' }))
+                break
+           
+            default:
+                break
+        }
+      }
+       // Verifier si un champ a une erreur pour l'afficher
+ function fieldHasError(field) {
+    if (errors[field] && errors[field] !== '') return true
+    return false
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target
+   validateField(name, value)
+    setState(previousState => ({ ...previousState, [name]: value }))
+  }
+
+
+
+
+
+    return (
+        <div>
+          <form className='container-inscription'>
+      <h1>Contactez moi</h1>
+
+<div className='form-group'>
+<label className='l1'>nom:</label>
+<input type='text' placeholder='Saisir votre nom'  name='nom'  className={`in1 ${fieldHasError('nom') && "is-invalid"}`} value={state.nom} onChange={handleChange}></input>
+<div className={fieldHasError('nom') ? "invalid-feedback" : "valid-feedback"}>{errors.nom}</div>
+  </div>     
+    
+     <div className='form-group'>
+     <label className='l2'>Prenom:</label>
+      <input type='text' placeholder='saisi votre prenom' name='prenom'  className={`in2 ${fieldHasError('nom') && "is-invalid"}`} value={state.prenom} onChange={handleChange}></input>
+      <div className={fieldHasError('prenom') ? "invalid-feedback" : "valid-feedback"}>{errors.prenom}</div>
+     </div>
+     <div className='form-group'>
+     <label className='l3'>Email:</label>
+      <input type='email' placeholder=' saisir votre emial' name='email'  className={`in3 ${fieldHasError('nom') && "is-invalid"}`} value={state.email} onChange={handleChange}></input>
+      <div className={fieldHasError('email') ? "invalid-feedback" : "valid-feedback"}>{errors.email}</div>
+      </div>
+     
+      <div className='form-group'>
+      <label className='l4'>Description:</label>
+      <textarea className={`in4 ${fieldHasError('description') && "is-invalid"}`} placeholder='Vous pouvez ecrire votre text ici' cols="20"></textarea>
+      </div>
+      <div className='form-group'>
+      <button type='submit'>Soumetre</button>
+      </div>
+      
+     
+      </form>   
+        </div>
+    );
+}
+
+
+export default contact
---
+++ app/temoinage/page.js
@@ -0,0 +1,95 @@
+'use client'
+import React, {useState, useEffect} from "react";
+
+const Avis = () => {
+
+   
+    const [state, setState]= useState({
+     firstName: '',
+     lastName: '',
+     email: '',
+     description: ''
+    })//valeur de chaque champs
+    const [opinion, setopinion]=useState([]);
+    function handleChange(event) {
+     const { name, value } = event.target
+   
+     
+     setState(previousState => ({ ...previousState, [name]: value }))
+   }
+   function submit(event) {
+    event.preventDefault() // Eviter le rafraichissement de la page ( eviter la soumission)
+    setopinion(previousopinion => [...previousopinion, { ...state, id: previousopinion.length + 1 }])
+    setState(prev => ({
+        ...prev,
+        nom: '',
+        prenom: '',
+        email: '',
+        description: ''
+       
+    }))  // Reinitialiser la forme apres la soumission
+  } 
+
+    return (
+        <div>
+          <form className='container-inscription'onSubmit={submit}>
+      <h1>Notez moi</h1>
+
+<div className='form-group'>
+<label className='l1'>nom:</label>
+<input type='text' placeholder='Saisir votre nom' required className='in1' name='firstname'   onChange={handleChange}></input>
+  </div>     
+    
+     <div className='form-group'>
+     <label className='l2'>Prenom:</label>
+      <input type='text' placeholder='saisi votre prenom'required className='in2' name="lastname"  onChange={handleChange} ></input>
+     </div>
+     <div className='form-group'>
+     <label className='l3'>Email:</label>
+      <input type='email' placeholder=' saisir votre emial' required className='in3'name="email" value={opinion.email} onChange={handleChange}></input>
+      </div>
+     
+      <div className='form-group'>
+      <label className='l4'>Description de l'avis:</label>
+      <textarea className='in4' placeholder='Vous pouvez ecrire votre text ici' cols="20" name="description"  value={opinion.description} on onChange={handleChange} ></textarea>
+      </div>
+      <div className='form-group'>
+      <button type='submit'>Appliquer mon avis</button>
+      </div>
+     
+      </form>
+      <>
+            {opinion.length ? (
+                <>
+                    <h1>Liste des utilisateurs ajoutes</h1>
+                    <table className='table table-striped'>
+                        <thead>
+                            <tr>
+                                <td>Id</td>
+                                <td>nom</td>
+                                <td>prenom</td>
+                                <td>Email</td>
+                                <td>description</td>
+                                <td>Actions</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {opinion.map(Avis => <tr key={Avis.id}>
+                                <td>{Avis.id}</td>
+                                <td>{Avis.prenom}</td>
+                                <td>{Avis.nom}</td>
+                                <td>{Avis.email}</td>
+                                <td>{Avis.description}</td>
+                                <td><button className='btn btn-primary' >Editer</button> <button className='btn btn-danger' >Supprimer</button></td>
+                            </tr>)}
+                        </tbody>
+                    </table>
+                </>):''}
+
+
+      </>  
+        </div>
+    );
+}
+
+export default Avis;
